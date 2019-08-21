4272f58c849f66b2be10dbf53bbf01cbda46ab31
manvi-sood <manvi.sood.97@gmail.com>
2019-08-21 15:31:41 -0400

    gitignor and day1.js added

---
+++ day1.js
@@ -0,0 +1,15 @@
+var ch1=require('chalk');
+
+
+console.log(ch1.blue("Example 1:")+ ch1.yellow("OR case with numbers 1 | 0"));
+if(1||0)
+{
+    console.log(ch1.green('truthy'));
+}
+else 
+{
+    console.log(ch1.red("falsy"));
+}
+
+
+
