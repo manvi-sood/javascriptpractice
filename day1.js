7b4fe02c4f219632d7d1e4791eab3228af32fc6e
manvi-sood <manvi.sood.97@gmail.com>
2019-08-21 15:43:13 -0400

    code beautified in day1.js

--- day1.js
+++ day1.js
@@ -1,15 +1,8 @@
-var ch1=require('chalk');
+var ch1 = require("chalk");
 
-
-console.log(ch1.blue("Example 1:")+ ch1.yellow("OR case with numbers 1 | 0"));
-if(1||0)
-{
-    console.log(ch1.green('truthy'));
+console.log(ch1.blue("Example 1:") + ch1.yellow("OR case with numbers 1 | 0"));
+if (1 || 0) {
+  console.log(ch1.green("truthy"));
+} else {
+  console.log(ch1.red("falsy"));
 }
-else 
-{
-    console.log(ch1.red("falsy"));
-}
-
-
-
